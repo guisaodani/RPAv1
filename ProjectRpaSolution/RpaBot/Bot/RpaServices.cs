947e90bc7fc7b003b92eade8007fc8f59bc807d0
guisaodani <guisaodani@gmail.com>
2026-04-09 21:56:54 -0500

    created initial config bot for open url dian

---
+++ ProjectRpaSolution/RpaBot/Bot/RpaServices.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Playwright;
+using RpaBot.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpaBot.Bot
+{
+    public class RpaServices
+    {
+        private IPage? _page;
+        private IBrowser? _browser;
+
+        public async Task StarAsync()
+        {
+            var playwright = await Playwright.CreateAsync();
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = !Settings.ShowBrowser
+            });
+            _page = await _browser.NewPageAsync();
+        }
+
+        public async Task GoDianAsync()
+        {
+            await _page!.GotoAsync(Settings.UrlDian);
+            await _page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            Console.WriteLine("Pagina cargada");
+        }
+
+        public async Task CloseAsyns()
+        {
+            await _browser!.CloseAsync();
+        }
+    }
+}
---
+++ ProjectRpaSolution/RpaBot/Config/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpaBot.Config
+{
+    public class Settings
+    {
+        public const string UrlDian = "https://muisca.dian.gov.co/WebRutMuisca/DefConsultaEstadoRUT.faces";
+        public const bool ShowBrowser = false;
+    }
+}
